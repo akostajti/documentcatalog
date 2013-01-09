ef05f6512ca0922c121ee5f71a3371d40c0a810d
akostajti <akos.tajti@gmail.com>
2013-01-09 18:58:31 +0100

    added some new package-info classes

---
+++ DocumentCatalog/src/net/docca/backend/convert/hocr/elements/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright by Akos Tajti (akos.tajti@gmail.com)
+ *
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of Akos Tajti. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with Akos Tajti.
+ */
+/**
+ * Contains classes representing hocr elements.
+ */
+package net.docca.backend.convert.hocr.elements;
+
