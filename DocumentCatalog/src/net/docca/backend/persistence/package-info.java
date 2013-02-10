b066ea584034a222dcda39ae69f16830f0f81497
akostajti <akos.tajti@gmail.com>
2013-02-10 14:01:11 +0100

    issue 22
    added the new dependencies (hibernate and the mysql connector)

---
+++ DocumentCatalog/src/net/docca/backend/persistence/package-info.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright by Akos Tajti (akos.tajti@gmail.com)
+ *
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of Akos Tajti. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with Akos Tajti.
+ */
+/**
+ * contains all persistence related classes (including finders, entity definitions etc.).
+ *
+ * @author Akos Tajti <akos.tajti@gmail.com>
+ *
+ */
+package net.docca.backend.persistence;
