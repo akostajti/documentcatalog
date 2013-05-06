283738e0ae9b0a564a339ba1d1667db2d68600da
akostajti <akos.tajti@gmail.com>
2013-05-06 17:13:02 +0200

    added a working login page

---
+++ DocumentCatalog/src/net/docca/backend/web/controllers/LoginController.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright by Akos Tajti (akos.tajti@gmail.com)
+ *
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information
+ * of Akos Tajti. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with Akos Tajti.
+ */
+package net.docca.backend.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Akos Tajti <akos.tajti@gmail.com>
+ *
+ */
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	/**
+	 * shows the upload form.
+	 * @return the name of the upload view
+	 */
+	@RequestMapping
+	public String show() {
+		return "login";
+	}
+}
